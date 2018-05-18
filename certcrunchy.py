a2a269f42faa2acc6e0a4ed87ef542796b7a85ff
joda32 <willem.mouton@gmail.com>
2018-05-18 12:08:48 +0200

    Added initial censys.io api checks. and it is starting to get useful now.

--- certcrunchy.py
+++ certcrunchy.py
@@ -9,7 +9,7 @@ import argparse
 import dns.resolver
 import threading
 import time
-import logging
+import re
 import queue
 from time import sleep
 from tempfile import mkstemp
@@ -28,9 +28,20 @@ _banner = """\033[1;33;40m
 """
 
 _transparency_endpoint = "https://crt.sh/?q=%.{query}&output=json"
+_censys_endpoint = "https://www.censys.io/api/v1"
+_censys_uid = None
+_censys_secret = None
 _potential_hosts = []
 _resolving_hosts = {}
 
+
+def is_valid_hostname(hostname):
+    if len(hostname) > 253:
+        return False
+    if hostname[-1] == ".":
+        hostname = hostname[:-1] # strip exactly one dot from the right, if present
+    allowed = re.compile("(?!-)[A-Z\d-]{1,63}(?<!-)$", re.IGNORECASE)
+    return all(allowed.match(x) for x in hostname.split("."))
 
 def getSubjectAltNames(_potential_host):
     result = []
@@ -94,6 +105,43 @@ class dnsThread(threading.Thread):
                 print(ex)
 
 
+def getCensysNames(domain):
+    page = 1
+    QUERY = "{{\"query\":\"{domain}\",\"page\":{page},\"fields\":[\"parsed.subject_dn\", \"ip\"],\"flatten\":true}}"
+    hosts = []
+    try:
+        while 1:
+            print("getting page {page}".format(page=page))
+            data = QUERY.format(domain=domain, page=page)
+            res = requests.post(_censys_endpoint + "/search/certificates", data=data, auth=(_censys_uid, _censys_secret))
+            if res.status_code != 200:
+                print("error occurred: {error}".format(res.json()["error"]))
+                break
+
+            for r in res.json()["results"]:
+                if "CN" in r["parsed.subject_dn"]:
+                    # There is some weirdness with some CN's not being propperly parsed, thus getting some shit output
+                    name = r["parsed.subject_dn"].split("CN=")[1].lower()
+                    if name.find(",") > -1:
+                        name = name.split(",")[0]
+                    if is_valid_hostname(name):
+                        if not name.find("*") == 0:
+                            if name.find("." + domain) > -1:
+                                hosts.append(name)
+
+            if len(res.json()["results"]) < 100:
+                break
+            page += 1
+            if page == 101:
+                print("Can't go past page 100")
+                break
+
+    except Exception as ex:
+        print(ex)
+    hosts = list(set(hosts))
+    return hosts
+
+
 def getTransparencyNames(domain):
     results = []
     print("Checking [{domain}]".format(domain=domain))
@@ -110,7 +158,6 @@ def getTransparencyNames(domain):
 
     results = list(set(results))
     results.sort()
-    #print("Found [{count}] potential hostnames".format(count=len(results)))
     return results
 
 
@@ -123,7 +170,12 @@ if __name__ == "__main__":
     parser.add_argument('-o', '--output', type=str, help="Results file")
     parser.add_argument('-t', '--delay', type=int, help="Delay between quering online services", default=3)
     parser.add_argument('-p', '--port', type=int, help="Port to connect to for SSL cert", default=443)
+    parser.add_argument('-U', '--uid', type=str, help="Censys.io UID")
+    parser.add_argument('-S', '--secret', type=str, help="Censys.io Secret")
     args = parser.parse_args()
+
+    _censys_uid = args.uid
+    _censys_secret = args.secret
 
     if not args.domain and not args.domains and not args.iprange:
         print("Requires either domain, domain list or ip range")
@@ -132,86 +184,44 @@ if __name__ == "__main__":
     if args.domain:
         print("Checking transparency archive for potential hostnames")
         _potential_hosts = getTransparencyNames(args.domain)
+        if _censys_uid and _censys_secret:
+            _potential_hosts = _potential_hosts + getCensysNames(args.domain)
 
     if args.domains:
         for domain in open(args.domains).read().split("\n"):
             _potential_hosts = _potential_hosts + getTransparencyNames(domain)
+            if _censys_uid and _censys_secret:
+                _potential_hosts = _potential_hosts + getCensysNames(args.domain)
             sleep(args.delay)
 
-print("Checking potential hostnames for DNS A records")
-threadCount = 20
+    print("Checking potential hostnames for DNS A records")
+    threadCount = 20
 
-threads = []
-q = queue.Queue()
-for h in _potential_hosts:
-    q.put(h)
-r = queue.Queue()
-
-#for _potential_host in _potential_hosts:
-#    ips = getARecIPs(_potential_host)
-#    if ips:
-
-for i in range(threadCount):
-    worker = dnsThread(q, r)
-    worker.setDaemon(True)
-    worker.start()
-    threads.append(worker)
-
-while not q.empty():
-    time.sleep(1)
-
-for worker in threads:
-    worker.stop()
-
-for worker in threads:
-    worker.join()
-
-for _host in list(r.queue):
-    _resolving_hosts[_host["host"]] = _host["ips"]
-
-print("Found [{count}] resolving hostnames".format(count=len(_resolving_hosts)))
-for _host in _resolving_hosts:
-    print("  {host} => [{ips}]".format(host=_host, ips=", ".join(_resolving_hosts[_host])))
-print("")
-
-print("Checking resolved hostnames for additional subjectAltname hosts")
-_potential_hosts = []
-for _host in _resolving_hosts:
-    _h = getSubjectAltNames(_host)
-    if (_h):
-        _potential_hosts = _potential_hosts + getSubjectAltNames(_host)
-_potential_hosts = list(set(_potential_hosts))
-_potential_hosts.sort()
-
-print("Found [{count}] SubjectAltNames".format(count=len(_potential_hosts)))
-print("Checking for previously unseen hostnames")
-
-threadCount = 20
-
-threads = []
-q = queue.Queue()
-for _host in _potential_hosts:
-    if not _resolving_hosts.get(_host):
+    threads = []
+    q = queue.Queue()
+    for h in _potential_hosts:
         q.put(h)
-r = queue.Queue()
+    r = queue.Queue()
 
-for i in range(threadCount):
-    worker = dnsThread(q, r)
-    worker.setDaemon(True)
-    worker.start()
-    threads.append(worker)
+    for i in range(threadCount):
+        worker = dnsThread(q, r)
+        worker.setDaemon(True)
+        worker.start()
+        threads.append(worker)
 
-while not q.empty():
-    time.sleep(1)
+    while not q.empty():
+        time.sleep(1)
 
-for worker in threads:
-    worker.stop()
+    for worker in threads:
+        worker.stop()
 
-for worker in threads:
-    worker.join()
+    for worker in threads:
+        worker.join()
 
-for _host in list(r.queue):
-    _resolving_hosts[_host["host"]] = _host["ips"]
+    for _host in list(r.queue):
+        _resolving_hosts[_host["host"]] = _host["ips"]
 
-for _host in _resolving_hosts:
-    print("  {host} => [{ips}]".format(host=_host, ips=", ".join(_resolving_hosts[_host])))
+    print("Found [{count}] resolving hostnames".format(count=len(_resolving_hosts)))
+    for _host in _resolving_hosts:
+        print("  {host} => [{ips}]".format(host=_host, ips=", ".join(_resolving_hosts[_host])))
+    print("")
