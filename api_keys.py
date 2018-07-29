5944f1173ec6a6ecbc20226a1e26b310f46f01d9
joda32 <willem.mouton@gmail.com>
2018-07-29 21:42:23 +0200

    Reset api_keys.py

--- api_keys.py
+++ api_keys.py
@@ -1,5 +1,5 @@
 _censys_uid = None
 _censys_secret = None
-_virustotal = "d67b82cf2e407cc4b94b5df5a8ddf344161b3cce9023de08e1a7fa11964c07a5"
-_riskiq_user = "willem@0ut.co.za"
-_riskiq_key = "67d5784dc37f08283b78c604ece0a37092407ed1cf76056ed6af4a974557311e"
+_virustotal = None
+_riskiq_user = None
+_riskiq_key = None
